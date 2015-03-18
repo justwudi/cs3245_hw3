d06a4a3848750bd4896116434994acc4540f8ca1
Wu Di <justwudi@gmail.com>
2015-03-18 18:10:06 +0800

    Update index.py to my liking

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ import pickle
 import string
 
 IGNORE_STOPWORDS = False
+stopwords = set()
 if IGNORE_STOPWORDS:
     stopwords = set(nltk.corpus.stopwords.words('english'))
 
@@ -27,7 +28,9 @@ def main():
     build_index(directory_path, dictionary_file_name, postings_file_name)
 
 
-def build_index(directory_path='/home/blym/nltk_data/corpora/reuters/training/', dictionary_file_name='dictionary.txt', postings_file_name='postings.txt'):
+def build_index(directory_path='/Users/WD/nltk_data/corpora/reuters/training/',
+                dictionary_file_name='dictionary.txt',
+                postings_file_name='postings.txt'):
     # Sort file names numerically, not lexicographically
     doc_file_names = os.listdir(directory_path)
     doc_file_names.sort(key=int)
@@ -40,54 +43,49 @@ def build_index(directory_path='/home/blym/nltk_data/corpora/reuters/training/',
     # Build postings lists; treat doc file names as doc ids
     for doc_file_name in doc_file_names:
         doc_file_path = os.path.join(directory_path, doc_file_name)
-        doc = open(doc_file_path, 'r')
 
-        # Add doc id to universal set
-        postings_lists[UNIVERSAL_SET_KEY].append(int(doc_file_name))
+        with open(doc_file_path, 'r') as doc:
+            # Add doc id to universal set
+            postings_lists[UNIVERSAL_SET_KEY].append(int(doc_file_name))
 
-        seen_terms = set()
+            seen_terms = set()
 
-        for line in doc:
-            tokens = nltk.word_tokenize(line)
+            for line in doc:
+                tokens = nltk.word_tokenize(line)
+                tokens = map(lambda t: stemmer.stem(t.lower()), tokens)
+                # filter out punctuations
+                terms = filter(lambda t: t not in PUNCTUATION and
+                               t not in stopwords, tokens)
 
-            # terms is an array of stemmed lowercase tokens that are not
-            # punctuation, or stopwords if IGNORE_STOPWORDS is true
-            terms = [stemmer.stem(token.lower())
-                     for token in tokens if token not in PUNCTUATION and not (IGNORE_STOPWORDS and token in stopwords)]
+                for term in terms:
+                    # Avoid duplicate doc ids for the same term
+                    if term not in seen_terms:
+                        seen_terms.add(term)
 
-            for term in terms:
-                # Avoid duplicate doc ids for the same term
-                if term not in seen_terms:
-                    seen_terms.add(term)
+                        # Initialise postings list for new term
+                        if term not in postings_lists:
+                            postings_lists[term] = []
 
-                    # Initialise postings list for new term
-                    if term not in postings_lists:
-                        postings_lists[term] = []
-
-                    # Add doc id to postings list
-                    postings_lists[term].append(int(doc_file_name))
-
-        doc.close()
+                        # Add doc id to postings list
+                        postings_lists[term].append(int(doc_file_name))
 
     # Stores start/end pointers to postings list within postings file
     # To be written to the dictionary file
     ptr_dictionary = {}
 
     # Stores postings lists
-    postings_file = open(postings_file_name, 'w')
+    with open(postings_file_name, 'w') as postings_file:
 
-    # Write postings lists to file, storing start/end pointers
-    for term, postings_list in postings_lists.iteritems():
-        start_ptr = postings_file.tell()
+        # Write postings lists to file, storing start/end pointers
+        for term, postings_list in postings_lists.iteritems():
+            start_ptr = postings_file.tell()
 
-        postings_list = pickle.dumps(postings_list)
-        postings_file.write(postings_list)
+            postings_list = pickle.dumps(postings_list)
+            postings_file.write(postings_list)
 
-        end_ptr = postings_file.tell()
+            end_ptr = postings_file.tell()
 
-        ptr_dictionary[term] = (start_ptr, end_ptr)
-
-    postings_file.close()
+            ptr_dictionary[term] = (start_ptr, end_ptr)
 
     # Write dictionary to file
     with open(dictionary_file_name, 'w') as dictionary_file:
